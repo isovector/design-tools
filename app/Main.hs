777b9f4533b110b4d43a1758db0ce81ac41e966f
Sandy Maguire <sandy@sandymaguire.me>
2019-10-26 20:37:15 +0200

    expand annotations

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,6 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = passes
+  [ expandAnnotations
+  ]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE LambdaCase #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Text.Pandoc
+import Text.Pandoc.Walk
+import Text.Pandoc.JSON
+import Data.Monoid
+
+
+expandAnnotations :: Pandoc -> Pandoc
+expandAnnotations = walk $ \case
+  Link _ [Str t] ("Ann", _) ->
+    RawInline (Format "latex") $ "\\ann{" ++ t ++ "}"
+  t -> t
+
+
+passes :: [Pandoc -> Pandoc] -> IO ()
+passes = toJSONFilter . appEndo . foldMap Endo
+
+
