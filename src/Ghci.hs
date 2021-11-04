afab9218924e2b383632f514c92f4abf46ff45f3
Sandy Maguire <sandy@sandymaguire.me>
2021-11-04 10:41:56 -0700

    Thinking-with-types compliant GHCI output parsing

--- src/Ghci.hs
+++ src/Ghci.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE LambdaCase   #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wall     #-}
 
 module Ghci (emitGhci, citeLaw) where
 
 import           Cache
-import           Combinators
 import           Control.Lens
 import           Data.Bool
 import           Data.Char
@@ -67,9 +67,11 @@ parseReplace x =
       _ -> error $ "invalid replace string: " <> T.unpack t
 
 emitGhci :: Block -> IO Block
+-- GHCI block
 emitGhci (CodeBlock (_, _, kvs) str)
   | Just file <- lookup "ghci" kvs
   = caching (file, str, kvs) $ ghciToPandoc kvs (T.unpack file) (T.unpack str)
+-- design block
 emitGhci (CodeBlock attr@(_, _, kvs) str)
   | Just file <- lookup "design" kvs
   = caching (file, str, attr, kvs) $ do
@@ -80,9 +82,11 @@ emitGhci (CodeBlock attr@(_, _, kvs) str)
         attr
         (T.unpack file) (
         T.unpack str)
+-- quickspec block without a reference id
 emitGhci (CodeBlock ("", _, kvs) str)
   | Just file <- lookup "quickspec" kvs
   = caching (file, str, kvs) $ fmap snd $ quickspecToPandoc kvs (T.unpack file) (T.unpack str)
+-- quickspec block with a reference id
 emitGhci (CodeBlock (xid, _, kvs) str)
   | Just file <- lookup "quickspec" kvs
   = do
@@ -98,7 +102,7 @@ emitGhci x = pure x
 -- | Run a function defined in the module, parsing its output as markdown
 designHashToPandoc
     :: String -> [(Text, Text)] -> Attr -> FilePath -> String -> IO Block
-designHashToPandoc fn kvs attr fp txt = do
+designHashToPandoc fn _ attr fp txt = do
   let hash = hashFile (fp, txt, attr)
   r <- runGhciVal fp
       $ unwords
@@ -153,9 +157,9 @@ format
 
 runGhci :: [(Text, Text)] -> ([String] -> [String]) -> FilePath -> String -> IO [(String, String)]
 runGhci kvs f fp str
-  = fmap (zip (lines $ replace str) . responses f . replace)
+  = fmap (interleave replace (lines $ replace str) . responses f . replace)
   . readProcess "stack" ["repl", "--no-load"]
-  $ unlines [":l " ++ fp, str]
+  $ unlines [":l " ++ fp, unlines $ fmap removeSilent $ lines $ str]
  where replace = doReplace $ lookup "replace" kvs
 
 runGhciVal :: FilePath -> String -> IO String
@@ -163,6 +167,11 @@ runGhciVal fp str
   = fmap (valResponse $ length $ lines str)
   . readProcess "stack" ["repl", "--no-load"]
   $ unlines [":l " ++ fp, ":{", str, ":}"]
+
+
+removeSilent :: String -> String
+removeSilent ('@':s) = s
+removeSilent s = s
 
 
 valResponse :: Int -> String -> String
@@ -197,4 +206,54 @@ removeManyTags ts = bool ts (removeManyTags $ removeTag ts) $ isResponse ts
 isResponse :: String -> Bool
 isResponse ('*':_) = True
 isResponse _ = False
+
+
+isSilent :: String -> Bool
+isSilent str
+  | isPrefixOf ":set "     str = True
+  | isPrefixOf "let "      str = True
+  | isPrefixOf "type "     str = True
+  | isPrefixOf "import "   str = True
+  | isPrefixOf "default (" str = True
+  | otherwise = isReallySilent str
+
+------------------------------------------------------------------------------
+-- | Zip input lines to GHCI with responses from GHCI. Correctly deals with
+-- input like @let x = 5@ which doesn't give a response.
+interleave :: (String -> String) -> [String] -> [String] -> [(String, String)]
+interleave f as
+  = filter (not . null . fst)
+  . zipping
+      isSilent
+      (\a ->
+        let a' = f a
+         in if isReallySilent a'
+               then ("", "")
+               else (a', ""))
+      (\a b -> (f a, initNonEmpty $ f b))
+      (fmap (dropWhile isSpace . f) as)
+
+
+initNonEmpty :: [a] -> [a]
+initNonEmpty [] = []
+initNonEmpty a = init a
+
+
+------------------------------------------------------------------------------
+-- | Input that starts wih a @\@@ is considered really silent, and doesn't even
+-- appear as prompted input.
+isReallySilent :: String -> Bool
+isReallySilent str
+  | isPrefixOf "@" str = True
+  | otherwise = False
+
+
+------------------------------------------------------------------------------
+-- | Probably the worst function I've ever written. I don't know what this
+-- does. Sorry!
+zipping :: (a -> Bool) -> (a -> c) -> (a -> b -> c) -> [a] -> [b] -> [c]
+zipping _ _ _ [] _ = []
+zipping _ _ _ _ [] = []
+zipping p d f (a:as) bs | p a = d a   : zipping p d f as bs
+zipping p d f (a:as) (b:bs)   = f a b : zipping p d f as bs
 
