eeb924ff5449fc0335582951b9bc1fdb1a3a4452
Sandy Maguire <sandy@sandymaguire.me>
2023-08-20 09:50:03 -0700

    indent only subsequent paragraphs

--- app/Main.hs
+++ app/Main.hs
@@ -108,10 +108,10 @@ flatten = concatMap $ \case
   t -> pure t
 
 noIndent :: Format -> [Block] -> [Block]
-noIndent f@(Format "latex") (c@CodeBlock{} : p@(Para pc@(Str str : _)) : xs) =
-    c : (Para $ RawInline (Format "latex") "\\noindent" : Space : pc) : noIndent f xs
-noIndent f@(Format "latex") (c@(Para [Math DisplayMath _]) : p@(Para pc@(Str str : _)) : xs) =
-    c : (Para $ RawInline (Format "latex") "\\noindent" : Space : pc) : noIndent f xs
+noIndent f@(Format "latex") (p1@Para{} : p2@Para{} : xs) =
+    p1 : noIndent f (p2 : xs)
+noIndent f@(Format "latex") (p1 : p2@(Para pc@(Str str : _)) : xs) =
+    p1 : (Para $ RawInline (Format "latex") "\\noindent" : Space : pc) : noIndent f xs
 noIndent f@(Format "latex") (x : xs) = x : noIndent f xs
 noIndent _ t = t
 
